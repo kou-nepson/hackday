3f2ab7dff12bf892d6378f1e70b020f18d4c6295
杉山航 <S-jonnepuson@s-sugiyamakou-no-macbook-pro.local>
2017-05-24 13:14:11 +0900

    Initial Commit

---
+++ TechtaGram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechtaGram
+//
+//  Created by 杉山航 on 2017/05/24.
+//  Copyright © 2017年 杉山航. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
