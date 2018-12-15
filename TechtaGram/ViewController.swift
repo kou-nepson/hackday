6c83e7fc88f57bae217aed422b08133fc6d1a01b
杉山航 <cjgjc803@gmail.com>
2018-12-15 18:37:40 +0900

    for kntk

--- TechtaGram/ViewController.swift
+++ TechtaGram/ViewController.swift
@@ -7,9 +7,23 @@
 //
 
 import UIKit
+//写真をSNSに投稿したいときに必要なフレームワーク
+import Accounts
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet var cameraImageView: UIImageView!
+    
+    let maskImage = UIImage(named: "IMG_5987.JPG")
+    
+    //画像を加工するための元となる画像
+    var oliginalimage: UIImage!
+    
+    var text = "🌟航🌟"
+    
+    //画像加工するフィルターの宣言
+    var filter: CIFilter!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +33,267 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //撮影するボタンのメソッド
+    @IBAction func takePhoto() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            
+            //カメラを起動
+            let picker = UIImagePickerController()
+            picker.sourceType = .camera
+            picker.delegate = self
+            picker.allowsEditing = true
+            //画面遷移
+            present(picker, animated: true, completion: nil)
+        } else {
+            //カメラを使えない時はコンソールに出ます
+            print("Error")
+        }
+    }
+    
+    //編集した画像を保存するボタンのメソッド
+    @IBAction func savePhoto() {
+        UIImageWriteToSavedPhotosAlbum(cameraImageView.image!, nil, nil, nil)
+    }
+    
+    //表示している画像にフィルタ加工するメソッド
+  /*
+    @IBAction func collarfilter() {
+        let filterImage: CIImage = CIImage(image: oliginalimage)!
+        
+        //フィルターの設定
+        filter = CIFilter(name: "CIColorControls")!
+        filter.setValue(filterImage, forKey: kCIInputImageKey)
+        //彩度の調整
+        filter.setValue(1.0, forKey: "inputSaturation")
+        //明度の調整
+        filter.setValue(0.5, forKey: "inputBrightness")
+        //コントラストの調整
+        filter.setValue(2.5, forKey: "inputContrast")
+        
+        let ctx = CIContext(options: nil)
+        let cgImage = ctx.createCGImage(filter.outputImage!, from: filter.outputImage!.extent)
+        cameraImageView.image = UIImage(cgImage: cgImage!)
+        
+        
+    }
+    
+    */
+    /*
+    //カメラロールにある画像を読み込む時のメソッド
+    @IBAction func openAlbam() {
+        //カメラロールが使えるか確認
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            
+            //カメラロールの画像を選択して画像を表示するまでの一連の流れ
+            let picker = UIImagePickerController()
+            picker.sourceType = .photoLibrary
+            picker.delegate = self
+            
+            picker.allowsEditing = true
+            
+            present(picker, animated: true, completion: nil)
+        }
+    }
+    */
+    //SNSに編集した画像を投稿する時のメソッド
+    @IBAction func snsPhoto() {
+        
+        //投稿するときに一緒に載せるコメント
+        let shareText = "写真加工いえい"
+        
+        //投稿する画像の選択
+        let shareImage = cameraImageView.image!
+        
+        //投稿する画像とテキストを用意
+        let activityItem: [Any] = [shareText,shareImage]
+        
+        let activityViewController = UIActivityViewController(activityItems: activityItem, applicationActivities: nil)
+        
+        let excludedActivityTypes = [UIActivityType.postToWeibo, .saveToCameraRoll, .print]
+        
+        activityViewController.excludedActivityTypes = excludedActivityTypes
+        
+        present(activityViewController, animated: true, completion: nil)
+        
+    }
+    // 合成ボタンを押したときに呼ばれるメソッド
+    @IBAction func processButtonTapped(_ sender: UIButton) {
+        
+        
+        // photoImageView.imageがnilでなければselecttedPhotoに値が入る
+        guard let selectedPhoto = cameraImageView.image else {
+            
+            //nilならアラートを表示してメソッドを抜ける
+            simpleAlert("画像がありません")
+            return
+        }
+        
+        let alertController = UIAlertController(title: "合成するパーツを選択", message: nil, preferredStyle: .actionSheet)
+        let firstAction = UIAlertAction(title: "テキスト", style: .default) {
+            action in
+            
+            self.inputTextField()
+            // selectedPhotoにテキストを合成して画面に描き出す
+            //self.photoImegeView.image = self.drawText(selectedPhoto)
+        }
+        let secondAction = UIAlertAction(title: "犬", style: .default){
+            action in
+            
+            
+            // seletedPhotoに画像を合成して画面に描き出す
+            self.cameraImageView.image = self.drawMaskImage(selectedPhoto)
+        }
+        let cancelAction = UIAlertAction(title: "キャンセル", style:  .cancel, handler: nil)
+        
+        
+        alertController.addAction(firstAction)
+        alertController.addAction(secondAction)
+        alertController.addAction(cancelAction)
+        
+        
+        present(alertController, animated: true, completion: nil)
+    }
+
+    
+    //カメラ、カメラロールを使ったときに選択した画像をアプリ内に表示するためのメソッド
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        cameraImageView.image = info[UIImagePickerControllerEditedImage] as? UIImage
+        //加工する前の画像を元の画像として記録しておく
+        oliginalimage = cameraImageView.image
+        //画面遷移
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // 任意のメッセージとOKボタンを持つアラートのメソッド
+    func simpleAlert(_ titleeString: String) {
+        
+        
+        let alertController = UIAlertController(title: titleeString, message: nil, preferredStyle: .alert)
+        
+        
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        
+        
+        present(alertController,animated: true, completion: nil)
+        
+    }
+    func inputTextField(){
+        
+        let alert:UIAlertController = UIAlertController(title:"action",
+                                                        message: "alertView",
+                                                        preferredStyle:  UIAlertControllerStyle.alert)
+        
+        let cancelAction:UIAlertAction = UIAlertAction(title: "Cancel",
+                                                       style: UIAlertActionStyle.cancel,
+                                                       handler: {
+                                                        (action:UIAlertAction!) -> Void in
+                                                        
+                                                        
+                                                        print("Cancel")
+        })
+        let defaultAction:UIAlertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: {
+            (action:UIAlertAction!) -> Void in
+            let textFields:Array<UITextField>? = alert.textFields as Array<UITextField>?
+            if textFields != nil {
+                for textFields:UITextField in textFields! {
+                    self.text = textFields.text!
+                    self.cameraImageView.image = self.drawText(self.cameraImageView.image!)
+                    //各テキストにアクセス
+                    print(textFields.text)
+                }
+            }
+        })
+        alert.addAction(cancelAction)
+        alert.addAction(defaultAction)
+        
+        //textfiledの追加
+        alert.addTextField(configurationHandler: {(text1:UITextField!) -> Void in
+        })
+        
+        present(alert, animated: true, completion: nil)
+    }
+    func drawMaskImage(_ image: UIImage) ->UIImage {
+        
+        
+        //グラフィックスコンテキスト生成,編集を開始
+        UIGraphicsBeginImageContext(image.size)
+        
+        
+        //読み込んだ写真を書き出す
+        image.draw(in: CGRect(x: 0,y: 0, width: image.size.width, height: image.size.height))
+        
+        
+        
+        
+        
+        // 描き出す位置と大きさの設定　CGRectMake([左からのx座標]px, [上からのy座標]px, [横の長さ]px)
+        let offset: CGFloat = 50.0
+        let maskRect = CGRect(
+            x: image.size.width - maskImage!.size.width - offset,
+            y: image.size.height - maskImage!.size.height - offset,
+            width: maskImage!.size.width,
+            height: maskImage!.size.height
+        )
+        
+        
+        //maskRectで指定した範囲にmaskImage絵お書き出す
+        maskImage!.draw(in: maskRect)
+        
+        
+        //グラフィックスコンテキストの画像を取得
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        
+        //グラフィックスコンテキストの編集を終了
+        UIGraphicsEndImageContext()
+        
+        
+        return newImage!
+    }
+    //元の画像にテキストを合成するメソッド
+    func drawText(_ image: UIImage) -> UIImage {
+        //テキストの内容の設定
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+            self.dismiss(animated: true, completion: nil)
+            //　画像を出力
+            cameraImageView.image = image
+        }
+        
+        
+        
+        
+        //グラフィックスコンテキスト生成、編集を開始
+        UIGraphicsBeginImageContext(image.size)
+        
+        //読み込んだ写真を書き出す
+        image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height ))
+        
+        //書き出す位置と大きさの設定CGRectMake([左からの座標]px,　[縦の長さ]px, [横の長さ]px)
+        let textRect  = CGRect(x: 5, y: 5, width: image.size.width - 5, height: image.size.height - 5)
+        
+        // textFontAttributes: 文字の特性　　（フォント、カラー、スタイル）の設定
+        let textFontAttributes = [
+            NSFontAttributeName: UIFont.boldSystemFont(ofSize: 120),
+            NSForegroundColorAttributeName: UIColor.red,
+            NSParagraphStyleAttributeName: NSMutableParagraphStyle.default
+        ]
+        //textRectで指定した範囲にtextFontAttributesに従ってテキストを描き出す
+        text.draw(in: textRect,withAttributes:  textFontAttributes)
+        
+        //グラフィックスコンテキストの画像を取得
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        //グラフィックスコンテキストの編集を終了
+        UIGraphicsEndImageContext()
+        
+        
+        return newImage!
+        
+    }
+
 
 
 }
